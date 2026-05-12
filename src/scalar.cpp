d62e793ea794baf7c36a509596c231333bdc96fd
Leonardo Vida <lleonardovida@gmail.com>
2026-05-12 03:00:57 +0200

    fix release header compatibility (#19)

--- src/scalar.cpp
+++ src/scalar.cpp
@@ -6,8 +6,18 @@
 #include "duckdb/common/types/time.hpp"
 #include "duckdb/common/types/timestamp.hpp"
 #include "duckdb/common/types/value.hpp"
+#if __has_include("duckdb/common/vector/flat_vector.hpp")
+#include "duckdb/common/vector/flat_vector.hpp"
+#endif
+#if __has_include("duckdb/common/vector/list_vector.hpp")
 #include "duckdb/common/vector/list_vector.hpp"
+#endif
+#if __has_include("duckdb/common/vector/string_vector.hpp")
+#include "duckdb/common/vector/string_vector.hpp"
+#endif
+#if __has_include("duckdb/common/vector/struct_vector.hpp")
 #include "duckdb/common/vector/struct_vector.hpp"
+#endif
 #include "duckdb/common/vector_operations/binary_executor.hpp"
 #include "duckdb/common/vector_operations/ternary_executor.hpp"
 #include "duckdb/common/vector_operations/unary_executor.hpp"
